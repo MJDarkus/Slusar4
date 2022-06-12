d66c1512498d6a9c98da863fe5d92bab0cd33fb5
MJDarkus <99sashko99@gmail.com>
2022-06-12 21:02:15 +0300

    final

--- code.cpp
+++ code.cpp
@@ -37,7 +37,7 @@ f.close();
 MPI_Barrier(MPI_COMM_WORLD);
 for (i=0; i<m; i++) {
 for (j=0; j<n; j++){
-if (!rank) cout << array[i][j] << "        ";
+if (!rank) cout << array[i][j] << "\t";
 }
 if(!rank) cout <<endl;
 }
@@ -47,8 +47,8 @@ for (k=0; k<size; k++, l=0){
   if (k==rank){
     for(int c=0; c<r; c++){
       for(l=0; l<n; l++)
-        if (array[r*k+c][0] <array[r*k+c][l]) array[r*k+c][0]=array[r*k+c][l];
-          cout << "Process " << k<< " Max is " << array[r*k+c][0] <<endl;
+        if (array[r*k+c][0] <array[r*k+c][l])
+	 array[r*k+c][0]=array[r*k+c][l];
         if (local_max<array[r*k+c][0] )
           local_max=array[r*k+c][0];
       }
@@ -58,7 +58,7 @@ int full_max=0;
 MPI_Reduce(&local_max, &full_max, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD) ;
 
 if (full_max){
-cout <<"-------------------- \nOverall Max is "<< full_max << endl << "--------------------\n";
+cout <<"-------------------- \nMax is "<< full_max << endl << "--------------------\n";
 }
 MPI_Finalize();
 return EXIT_SUCCESS;
