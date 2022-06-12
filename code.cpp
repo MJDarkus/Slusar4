13a0e8d656647e0f41778a081a3343d9b5b005f5
MJDarkus <49124489+MJDarkus@users.noreply.github.com>
2022-06-12 21:13:57 +0300

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -22,24 +22,24 @@ MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 MPI_Comm_size(MPI_COMM_WORLD, &size);
 
 int r=atoi(argv[1]);
-m= r * size;
+m=r * size;
 n=m * 2;
 int array[m][n];
 
 ifstream f;
 for(i=0; i<m; i++){
-f.open("file" + to_string(i) + ".in", std::ifstream::in);
-for(j=0; j<n; j++){
-f>>array[i][j];
-}
-f.close();
+	f.open("file" + to_string(i) + ".in", std::ifstream::in);
+	for(j=0; j<n; j++){
+		f>>array[i][j];
+	}
+	f.close();
 }
 MPI_Barrier(MPI_COMM_WORLD);
 for (i=0; i<m; i++) {
-for (j=0; j<n; j++){
-if (!rank) cout << array[i][j] << "\t";
-}
-if(!rank) cout <<endl;
+	for (j=0; j<n; j++){
+		if (!rank) cout << array[i][j] << "\t";
+	}
+	if(!rank) cout <<endl;
 }
 int local_max;
 int temp_max=0;
@@ -58,7 +58,7 @@ int full_max=0;
 MPI_Reduce(&local_max, &full_max, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD) ;
 
 if (full_max){
-cout <<"-------------------- \nMax is "<< full_max << endl << "--------------------\n";
+	cout <<"-------------------- \nMax is "<< full_max << endl << "--------------------\n";
 }
 MPI_Finalize();
 return EXIT_SUCCESS;
